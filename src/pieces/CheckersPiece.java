e9e60f6983f4d473539bb09930ac3d0049e81b12
Chris <ckhedoo@gmail.com>
2015-09-14 00:43:25 -0400

    Implemented basic Checkers pieces

---
+++ src/pieces/CheckersPiece.java
@@ -0,0 +1,90 @@
+package pieces;
+
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+
+import CheckersBoard.CheckersBoard;
+
+public abstract class CheckersPiece {
+    public static final int NORMAL_MOVE_DIAGAONAL_LENGTH = 1;
+    public static final int JUMP_MOVE_DIAGONAL_LENGTH = 2;
+
+    private Color color;
+    private ImageIcon representation;
+    private int x, y;
+
+    public CheckersPiece(Color colorInit, ImageIcon representationInit, int xInit, int yInit) {
+        setColor(colorInit);
+        setRepresentation(representationInit);
+        setX(xInit);
+        setY(yInit);
+    }
+
+    public void updatePosition(int newX, int newY) {
+        setX(newX);
+        setY(newY);
+    }
+
+    public boolean move(CheckersBoard board, int moveToX, int moveToY) {
+        if (isPositionFreeToMoveTo(board, moveToX, moveToY)) {
+            if (isMoveNormal(moveToX, moveToY)) {
+                updatePosition(moveToX, moveToY);
+                return true;
+            }
+            if (isMoveJump(moveToX, moveToY)) {
+                
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isPositionFreeToMoveTo(CheckersBoard board, int moveToX, int moveToY) {
+        return board.isPositionFree(moveToX, moveToY);
+    }
+
+    public boolean isMoveNormal(int moveToX, int moveToY) {
+        return isMoveValid(moveToX, moveToY, NORMAL_MOVE_DIAGAONAL_LENGTH);
+    }
+
+    public boolean isMoveJump(int moveToX, int moveToY) {
+        return isMoveValid(moveToX, moveToY, JUMP_MOVE_DIAGONAL_LENGTH);
+    }
+
+    private boolean isMoveValid(int moveToX, int moveToY, int moveLength) {
+        return Math.abs(x - moveToX) == moveLength && Math.abs(y - moveToY) == moveLength;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public ImageIcon getRepresentation() {
+        return representation;
+    }
+
+    public void setRepresentation(ImageIcon representation) {
+        this.representation = representation;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
---
+++ src/pieces/KingCheckersPiece.java
@@ -0,0 +1,12 @@
+package pieces;
+
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+
+public class KingCheckersPiece extends CheckersPiece {
+
+    public KingCheckersPiece(Color colorInit, ImageIcon representationInit, int xInit, int yInit) {
+        super(colorInit, representationInit, xInit, yInit);
+    }
+}
